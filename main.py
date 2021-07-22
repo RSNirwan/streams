8c5a92dbea9cf6fbbfe1b9cd3679f85e3d6f7d87
RSNirwan <rajbir.nirwan@gmail.com>
2021-07-22 23:46:45 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
-def producer(inps, ts, sink, collector):
-    if ts:
-        target = ts[0](ts[1:], sink, collector)
-        next(target)
+from numbers import Number
+from typing import Callable, List, Union, Iterator
+from stream import Stream
+
+stream = Stream()
+
+
+@stream.source
+def source(inp: Union[Number, List[Number]]) -> Iterator:
+    if isinstance(inp, Number):
+        yield inp
     else:
-        target = sink(collector)
-        next(target)
-    try:
-        for inp in inps:
-            target.send(inp)
-    except GeneratorExit:
-        pass
+        yield from inp
 
 
-def transformer(ts, sink, collector):
-    if ts:
-        target = ts[0](ts[1:], sink, collector)
-        next(target)
-    else:
-        target = sink(collector)
-        next(target)
-    try:
-        while True:
-            rec = yield
-            rec = rec * rec
-            target.send(rec)
-    except GeneratorExit:
-        pass
+@stream.filter_transform
+def transform1(inp: Number) -> bool:
+    # forward only even numbers
+    return (inp - 1) % 2
 
 
-def sink(collector):
-    try:
-        while True:
-            rec = yield
-            collector.append(rec)
-    except GeneratorExit:
-        pass
+@stream.expand_transform
+def transform2(inp: Number) -> Iterator:
+    # double every input
+    yield from [inp, inp]
 
 
-inps = [1, 2, 3, 4]
-collector = []
-producer(inps, [transformer], sink, collector)
-print(collector)
+@stream.map_transform
+def transform3(inp: Number) -> Number:
+    # square inp
+    return inp * inp
+
+
+@stream.reduce_transform
+def transform4(inps: List[Number]) -> Number:
+    # sum every two numbers
+    condition = lambda: len(inps) >= 2
+    func = lambda: sum(inps)
+    return condition, func
+
+
+@stream.sink
+def sink(inp: Number, collector: Callable) -> None:
+    collector(inp)
+
+
+a = []
+collector = lambda x: a.append(x)
+stream(input=[1, 2, 3], collector=collector)  # even inputs are squared
+print(a)
+
+stream(input=4, collector=collector)
+stream(input=5, collector=collector)
+print(a)
+
+for i in [6, 7, 8]:
+    stream(input=i, collector=collector)
+print(a)
